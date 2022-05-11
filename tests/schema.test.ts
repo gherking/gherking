639e913c0bad1ad4b561aabbee1982fdf32453c1
Laszlo Szikszai <sziklaszlo@gmail.com>
2022-05-11 17:14:29 +0200

    Updated schema and added tests

---
+++ tests/schema.test.ts
@@ -0,0 +1,15 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+import { matchers } from "jest-json-schema";
+expect.extend(matchers);
+
+const readJSON = (p: string) => JSON.parse(readFileSync(join(__dirname, p), "utf-8"));
+const schema = readJSON("../schema/gherking.schema.json");
+
+describe(".gherking.json schema", () => {
+  test("should handle valid .gherking.json", () => {
+    const config = readJSON("./data/.gherking.json");
+
+    expect(config).toMatchSchema(schema);
+  });
+});
